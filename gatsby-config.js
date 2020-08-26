6f66c703b83906150d1e661273e68b8f189c9d9b
Spenser Saling <spenser0saling@gmail.com>
2020-08-26 08:28:34 -0700

    Modify Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Uni`,
+    description: `The Open Source Spoken Language`,
+    author: `The Uni Community`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
